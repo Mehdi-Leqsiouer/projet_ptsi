3107afe3df6f291d1de71654481a00d806715236
Mehdi <46991431+Mehdi-Leqsiouer@users.noreply.github.com>
2021-02-03 18:17:11 +0100

    Add files via upload

--- get_dijsktra.php
+++ get_dijsktra.php
@@ -1,7 +1,7 @@
 <?php
     $item=$_POST["depart"];
 	$item2=$_POST["arriver"];
-    $tmp = passthru("python calc_dijsktra.py $item $item2");
+    $tmp = passthru("python3 calc_dijsktra.py $item $item2");
     echo $tmp;
 
 ?>
